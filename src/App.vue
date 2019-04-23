e30b9d976510af3f9c736522afb16b2f4de0ab08
xyyojl <m13631897256@163.com>
2019-04-23 14:42:07 +0800

    初始化项目，实现hello Vue并注释 build/webpack.base.conf.js 里的ESLint

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <p>{{message}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data (){
+    return { message: 'hello Vue' }
+  }
+}
+</script>
+
+<style>
+  p{color:red;}
+</style>
